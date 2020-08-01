a2b25d59e852e41157280fe95a064fdc86e0f93e
DESKTOP-D2V1UJK\Danilshik <danilshik@yandex.ru>
2020-08-01 13:06:28 +0300

    refactor6

---
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -0,0 +1,6 @@
+package ru.skillbranch.skillarticles.ui.custom.behaviors
+
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import ru.skillbranch.skillarticles.ui.custom.ArticleSubmenu
+
+class SubmenuBehavior() : CoordinatorLayout.Behavior<ArticleSubmenu>()
