6ff413f03ef7c85a25850dbd70a250b5014d2fe0
danilshik <danilshik@yandex.ru>
2021-04-11 21:20:08 +0300

    Task 10/1

--- app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
@@ -24,10 +24,7 @@ import com.google.android.material.behavior.HideBottomViewOnScrollBehavior
 import kotlinx.android.synthetic.main.activity_root.*
 import ru.skillbranch.skillarticles.R
 import ru.skillbranch.skillarticles.extensions.dpToIntPx
-import ru.skillbranch.skillarticles.viewmodels.base.BaseViewModel
-import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
-import ru.skillbranch.skillarticles.viewmodels.base.NavigationCommand
-import ru.skillbranch.skillarticles.viewmodels.base.Notify
+import ru.skillbranch.skillarticles.viewmodels.base.*
 
 abstract class BaseActivity<T : BaseViewModel<out IViewModelState>> : AppCompatActivity() {
     protected abstract val viewModel: T
@@ -49,6 +46,7 @@ abstract class BaseActivity<T : BaseViewModel<out IViewModelState>> : AppCompatA
         viewModel.observeState(this) { subscribeOnState(it) }
         viewModel.observeNotifications(this) { renderNotification(it) }
         viewModel.observeNavigation(this) { subscribeOnNavigation(it) }
+        viewModel.observeLoading(this) { renderLoading(it)}
 
         navController = findNavController(R.id.nav_host_fragment)
     }
@@ -91,19 +89,24 @@ abstract class BaseActivity<T : BaseViewModel<out IViewModelState>> : AppCompatA
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    open fun renderLoading(loadingState: Loading){
+        when (loadingState) {
+            Loading.SHOW_LOADING -> progress.isVisible = true
+            Loading.SHOW_BLOCKING_LOADING -> {
+                progress.isVisible = true
+                //TODO block interact with UI
+            }
+            Loading.HIDE_LOADING -> progress.isVisible = false
+        }
+    }
 }
 
 class ToolbarBuilder {
-    var title: String? = null
     var subtitle: String? = null
     var logo: String? = null
     var visibility: Boolean = true
     val items: MutableList<MenuItemHolder> = mutableListOf()
-
-    fun setTitle(title: String): ToolbarBuilder {
-        this.title = title
-        return this
-    }
 
     fun setSubtitle(subtitle: String): ToolbarBuilder {
         this.subtitle = subtitle
@@ -126,7 +129,6 @@ class ToolbarBuilder {
     }
 
     fun invalidate(): ToolbarBuilder {
-        this.title = null
         this.subtitle = null
         this.logo = null
         this.visibility = true
@@ -145,7 +147,6 @@ class ToolbarBuilder {
         context.appbar.setExpanded(true, true)
 
         with(context.toolbar) {
-            if (this@ToolbarBuilder.title != null) title = this@ToolbarBuilder.title
             subtitle = this@ToolbarBuilder.subtitle
             if (this@ToolbarBuilder.logo != null) {
                 val logoSize = context.dpToIntPx(40)
